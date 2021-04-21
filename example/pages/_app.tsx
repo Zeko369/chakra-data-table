8c036d237599c7ca247765188692ec1cb31b96d7
Fran Zekan <zekan.fran369@gmail.com>
2021-04-21 18:28:53 +0200

    Setup better example

--- example/pages/_app.tsx
+++ example/pages/_app.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import { ConfirmContextProvider } from 'chakra-confirm';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <ChakraProvider resetCSS>
-      <Component {...pageProps} />
+      <ConfirmContextProvider>
+        <Component {...pageProps} />
+      </ConfirmContextProvider>
     </ChakraProvider>
   );
 };
--- example/pages/index.tsx
+++ example/pages/index.tsx
@@ -1,40 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NextPage } from 'next';
-import {
-  Text,
-  HStack,
-  Button,
-  Table,
-  Tbody,
-  Td,
-  Th,
-  Thead,
-  Tr,
-  VStack
-} from '@chakra-ui/react';
+import { Text, HStack, Button, VStack, Heading } from '@chakra-ui/react';
+import { useConfirmDelete } from 'chakra-confirm';
+import { LinkButton } from 'chakra-next-link';
+
 import { DataTable } from '../../dist';
 
-const data = [
-  { id: 1, title: 'foobar', items: ['1', '2', '3'] },
-  { id: 2, title: 'something', items: [] },
-  { id: 3, title: 'fooasdasd', items: ['1', '2', '3'] },
-  { id: 4, title: null, items: ['1'] }
-];
-
 const Home: NextPage = () => {
+  const [data, setData] = useState([
+    { id: 1, title: 'foobar', items: ['1', '2', '3'] },
+    { id: 2, title: 'something', items: [] },
+    { id: 3, title: 'fooasdasd', items: ['1', '2', '3'] },
+    { id: 4, title: null, items: ['1'] }
+  ]);
+
+  const confirm = useConfirmDelete({
+    body: (
+      <VStack>
+        <Heading size="sm">Interested in this popup?</Heading>
+        <LinkButton size="sm" href="https://www.npmjs.com/package/chakra-confirm">
+          Check it out
+        </LinkButton>
+      </VStack>
+    )
+  });
+
   return (
     <DataTable
       title="Some random data"
       data={data}
       keys={['id', 'title', 'items', 'buttons'] as const}
+      keyFunc={'id'}
+      tableProps={{
+        td: {
+          color: 'blue.700'
+        },
+        thead: {
+          fontStyle: 'italic'
+        }
+      }}
+      showFooter
       mapper={{
         id: true,
         title: (r) => <Text color="green">{r.title}</Text>,
         items: (r) => r.items.join(', '),
-        buttons: () => (
+        buttons: (r) => (
           <HStack>
-            <Button colorScheme="green" size="sm">
-              Click me
+            <Button
+              onClick={() => {
+                confirm().then(() => setData((d) => d.filter((i) => i.id !== r.id)));
+              }}
+              colorScheme="red"
+              size="sm"
+            >
+              Delete me
             </Button>
           </HStack>
         )
